27516e9e432ebc9be1f545d4035ac618bdbe227a
BautistaGaber <bautistagaber@gmail.com>
2023-08-13 01:19:41 -0300

    add new changes

---
+++ script/character.js
@@ -0,0 +1,8 @@
+export class Character{
+    constructor(id, name, house, gender){
+      this.id = id
+      this.name = name
+      this.house = house
+      this.gender = gender
+    }
+  }
---
+++ script/charactersData.js
@@ -0,0 +1,13 @@
+import { Character } from './character.js'
+
+const Harry = new Character(1, 'Harry', 'Gryffindor', 'Male')
+
+const Hermione = new Character(2, 'Hermione', 'Gryffindor', 'Female')
+
+const Ron = new Character(3, 'Ron', 'Gryffindor', 'Male')
+
+const Draco = new Character(4, 'Draco', 'Slithering', 'Male')
+
+const Snape = new Character(5, 'Snape', 'Gryffindor', 'Male')
+
+export const characters = [Harry, Hermione, Ron, Draco, Snape]
