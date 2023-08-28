02477f7784104fb5798f80aacfc6c099caeea43e
BautistaGaber <bautistagaber@gmail.com>
2023-08-28 00:23:05 -0300

    add new attributes

--- script/character.js
+++ script/character.js
@@ -1,8 +1,14 @@
 export class Character{
-    constructor(id, name, house, gender){
+    constructor(id, name, species, house, gender, actor, patronus, hogwartsStudent,dateOfBirth,eyeColour ){
       this.id = id
       this.name = name
+      this.species = species
       this.house = house
       this.gender = gender
+      this.actor = actor
+      this.patronus = patronus
+      this.hogwartsStudent = hogwartsStudent
+      this.dateOfBirth = dateOfBirth
+      this.eyeColour = eyeColour
     }
   }
